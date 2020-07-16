79cfabc297a8218d9edc5e5bfecf59d32e1d091d
Oksyroksy <o.azarh@gmail.com>
2020-07-16 16:57:20 +0300

    JS_Base

---
+++ script.js
@@ -0,0 +1,45 @@
+
+        function checkDistance() {
+            if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+                btn.style.display = 'block';
+            } else {
+                btn.style.display = 'none';
+            }
+        }
+
+        function scrollToTop() {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+
+        var btn = document.createElement('button');
+        btn.id = 'toTop';
+        btn.innerText = ' TOP ';
+        btn.onclick = scrollToTop;
+        window.onscroll = checkDistance;
+        document.body.appendChild(btn);
+
+        var modal = document.querySelector('.modal');
+        var BtnShowModal = document.querySelector('.head-menu > li:last-child');
+        var btnClose = document.getElementById('closeBtn');
+        var shadow =  document.querySelector('.shadow');
+
+
+        BtnShowModal.addEventListener('click', function(){
+            modal.style.display = 'block';
+            shadow.style.display = 'block';
+            
+        });
+        btnClose.addEventListener('click', function(){
+            modal.style.display = 'none';
+            shadow.style.display = 'none';
+        } );
+
+            $(document).ready(function(){
+              $('.slider').slick({
+                autoplay: false,
+                dots: true,
+                mobileFirst: true,
+              });
+            });
+
+
